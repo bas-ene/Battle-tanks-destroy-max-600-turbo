4afdb8a856a012c2a1429dece46ca809846bffe1
evangerva <colger88@gmail.com>
2023-11-16 18:36:30 +0100

    client ascolta per tasti w-a-s-d e li manda al server

--- client/Client.java
+++ client/Client.java
@@ -1,6 +1,10 @@
 package client;
 import java.io.*;
 import java.net.*;
+
+import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 import tank_lib.*;
 
 /**
@@ -23,22 +27,72 @@ public class Client {
 			}
 		}
 		*/
+		Map m = new Map(20, 20);
+		Tank p1 = new Tank(new Point(250, 250), "you");
+		Tank p2 = new Tank(new Point(100, 100), "enemy");
+		BattleFrame f = new BattleFrame(m, p1, p2);
 		Client client = new Client();
-		client.startConnection("localhost", 12345);
+
+		client.startConnection("localhost", 12345, f);
+		
+		
 	}
 
-	public void startConnection(String ip, int port) {
+	public void startConnection(String ip, int port, BattleFrame f) {
 		try {
 			Socket socket = new Socket(ip, port);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-			// To send a message
-			out.println("Hello, server!");
+			f.addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent e) {
+					if (e.getKeyChar() == 'w') {
+						out.println("START_MOVING");
+						System.out.println("START_MOVING");
+					} else if (e.getKeyChar() == 's') {
+						out.println("START_BACK");
+						System.out.println("START_BACK");
+					} else if (e.getKeyChar() == 'a') {
+						out.println("START_LEFT");
+						System.out.println("START_LEFT");
+					} else if (e.getKeyChar() == 'd') {
+						out.println("START_RIGHT");
+						System.out.println("START_RIGHT");
+					}
+				}
 
+				public void keyReleased(KeyEvent e) {
+					if (e.getKeyChar() == 'w') {
+						out.println("STOP_MOVING");
+						System.out.println("STOP_MOVING");
+					} else if (e.getKeyChar() == 's') {
+						out.println("STOP_BACK");
+						System.out.println("STOP_BACK");
+					} else if (e.getKeyChar() == 'a') {
+						out.println("STOP_LEFT");
+						System.out.println("STOP_LEFT");
+					} else if (e.getKeyChar() == 'd') {
+						out.println("STOP_RIGHT");
+						System.out.println("STOP_RIGHT");
+					}
+				}
+			});
+
+			f.setFocusable(true);
+			f.requestFocusInWindow();
 			// To receive a message
 			String response = in.readLine();
 			System.out.println("Server says: " + response);
+			while (true) {
+
+				f.paint(f.getGraphics());
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- server/Server.java
+++ server/Server.java
@@ -8,16 +8,20 @@ public class Server {
 	public static void main(String[] args) {
 		try {
 			ServerSocket serverSocket = new ServerSocket(12345);
-			Socket clientSocket = serverSocket.accept();
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			System.out.println("Server started and listening on port 12345");
 
-			// To receive a message
-			String request = in.readLine();
-			System.out.println("Client says: " + request);
+			while (true) {
+				Socket clientSocket = serverSocket.accept();
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-			// To send a message
-			out.println("Hello, client!");
+				String request;
+				while ((request = in.readLine()) != null) {
+					System.out.println("Client says: " + request);
+					// To send a message
+					out.println("Hello, client!");
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
