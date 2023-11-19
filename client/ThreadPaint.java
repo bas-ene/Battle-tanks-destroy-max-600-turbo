c28543ac7c743f5c760b03f7650b6ee60399ca5e
bas-ene <enea.basilico@gmail.com>
2023-11-19 20:12:42 +0100

    aumentato fps, perchè la logica è separata

--- client/ThreadPaint.java
+++ client/ThreadPaint.java
@@ -17,7 +17,7 @@ public class ThreadPaint extends Thread{
 
             // 30 FPS
             try {
-                Thread.sleep(66);
+                Thread.sleep(33);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
